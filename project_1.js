467bc843395ba77113e945fe50899b3593b0466b
Pushkar Mondal <116779860+Pushkarmondal@users.noreply.github.com>
2024-01-09 15:24:25 +0530

    Add files via upload

---
+++ project_1.js
@@ -0,0 +1,12 @@
+/*this line crate a variable name kelvin and stores value of 293 and it is constant value.*/
+const kelvin = 0;
+/*in this below line we subtract kelvin value by 273 and store in celcius*/
+var celcius = kelvin - 273;
+/*this line we multiply celcius by (9/5) ans add 32 on it and stores this value into farhenheit*/
+let farhenheit = celcius * (9 / 5) + 32;
+/* in this line we convert decimal value into integer value*/
+farhenheit = Math.floor(farhenheit);
+console.log(`The temperature is ${farhenheit} degrees Farenheit`);
+
+
+
